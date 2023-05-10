6e1a314d2eff7179ffdc1f3e83c58bee4713819a
Stardust <2368354416@qq.com>
2023-05-10 11:06:54 +0800

    add

--- funcs.js
+++ funcs.js
@@ -15,7 +15,18 @@ export const calcPixel = text => {
   return Math.floor(number)
 }
 
+export const img2Base64 = selector => {
+  const img = typeof selector === 'string' ? $one(selector) : selector
+  const canvas = document.createElement('canvas')
+  canvas.width = img.width
+  canvas.height = img.height
+  const ctx = canvas.getContext('2d')
+  ctx.drawImage(img, 0, 0, canvas.width, canvas.height)
+  return canvas.toDataURL()
+}
+
 export default {
   isWindows,
-  calcPixel
+  calcPixel,
+  img2Base64
 }
